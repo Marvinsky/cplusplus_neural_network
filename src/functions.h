5fddba7905c87d18939459c9d0bed437f74579d1
Marvinsky <marvin.abisrror@gmail.com>
2019-11-17 10:39:24 -0500

    Neural Network created from scratch using c++

---
+++ src/functions.h
@@ -0,0 +1,16 @@
+
+
+#ifndef WORKSPACE_FUNCTIONS_H
+#define WORKSPACE_FUNCTIONS_H
+
+#include "tipos.h"
+
+void showVectorVals(string label, vector<double> &v) {
+    cout<<label<<" ";
+    for(unsigned i = 0; i < v.size(); ++i) {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+#endif //WORKSPACE_FUNCTIONS_H
---
+++ src/tipos.h
@@ -0,0 +1,19 @@
+
+#ifndef WORKSPACE_TIPOS_H
+#define WORKSPACE_TIPOS_H
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <cassert>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+//typedef string TipoString;
+using TipoDecimal = float;
+
+#endif //WORKSPACE_TIPOS_H
